cf489187ef866c219b5d5fefc55b7ee9e9a48385
Mkzay <80535194+Mkzay@users.noreply.github.com>
2023-08-05 16:58:58 +0100

    Update

--- Script-for-pages/app-2.js
+++ Script-for-pages/app-2.js
@@ -2,4 +2,19 @@ const backToOtherScreen = document.getElementById('back');
 
 backToOtherScreen.addEventListener('click', function(){
     window.location.href = 'Onboarding-Screen.html';
-})
+});
+
+const passWord = document.getElementById('password');
+const changeIcon = document.getElementById('changeIcon');
+
+changeIcon.addEventListener('click', function() {
+    if (passWord.type === 'text') {
+        passWord.type = 'password';
+        changeIcon.classList.remove('fa-eye-slash');
+        changeIcon.classList.add('fa-eye');
+    } else {
+        passWord.type = 'text';
+        changeIcon.classList.remove('fa-eye');
+        changeIcon.classList.add('fa-eye-slash');
+    }
+});
